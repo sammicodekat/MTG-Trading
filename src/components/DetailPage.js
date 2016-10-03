a37219f0af514a01fccfa74d969209bcf191761e
Donovan <donovan6925@gmail.com>
2016-10-02 17:48:19 -0700

    added Amazon link and more info to detail page

--- src/components/DetailPage.js
+++ src/components/DetailPage.js
@@ -36,7 +36,7 @@ export default class DetailPage extends Component {
 
   render() {
     let { card } = this.state;
-    let Name, imageUrl, type, power, toughness, rarity, setName , text, color, colorStr= '';
+    let Name, imageUrl, type, power, toughness, rarity, setName , text, color, colorStr, flavor, artist, cost, url = '';
 
     if(card) {
       // console.log('card:', card);
@@ -47,8 +47,12 @@ export default class DetailPage extends Component {
       toughness = card.card.toughness;
       rarity = card.card.rarity;
       setName = card.card.setName;
-      text = card.card.text
+      text = card.card.text;
       color = card.card.colors;
+      flavor = card.card.flavor;
+      artist = card.card.artist;
+      cost = card.card.manaCost;
+      url = `https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=mtg+${Name.split(' ').join('+')}`;
       if(color){
         color.length == 1 ? (colorStr=color[0]): (colorStr=color[0]+'/'+color[1]);
       }
@@ -61,15 +65,21 @@ export default class DetailPage extends Component {
           <div className ="col-xs-6 card card-container">
             <img src={imageUrl} alt={Name}/>
             <Link  to='/deck' className="btn btn-default button" onClick={this.addToDeck}>Add to Deck</Link>
+            <a href={url} target="_blank"><img src="https://1.bp.blogspot.com/-xXJU6yWkjkM/V14WGAKkAVI/AAAAAAAAIMw/mh1ykJweeBodMLUEcL_BxIcUNE860_NCQCLcB/s1600/amazon-buy-button-png1.png" className="amazon" alt="Buy on Amazon"/></a>
           </div>
           <div className ="col-xs-6 card card-container des">
             <ul className ="list-group">
               <li className = "list-group-item"><b>Type:</b> {type}</li>
               <li className = "list-group-item"><b>Power and Toughness:</b> {power}/{toughness}</li>
+              <li className = "list-group-item"><b>Cost: </b>{cost}</li>
               <li className = "list-group-item"><b>Colors: </b>{colorStr}</li>
               <li className = "list-group-item"><b>Rarity: </b>{rarity}</li>
               <li className = "list-group-item"><b>Set: </b>{setName}</li>
-              <li className = "list-group-item">{text}</li>
+              <li className = "list-group-item"><b>Text: </b>{text}</li>
+              <li className = "list-group-item"><b>Flavor: </b>{flavor}</li>
+              <li className = "list-group-item"><b>Artist: </b>{artist}</li>
+
+
             </ul>
           </div>
         </div>
