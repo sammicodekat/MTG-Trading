c3cc11cc95cfac91c4d8421d805b028a81c8f7e1
Sammi A <sammi.m.yin@gmail.com>
2016-10-02 18:48:52 -0700

    add cost img

--- src/components/DetailPage.js
+++ src/components/DetailPage.js
@@ -36,7 +36,9 @@ export default class DetailPage extends Component {
 
   render() {
     let { card } = this.state;
-    let Name, imageUrl, type, power, toughness, rarity, setName , text, color, colorStr= '';
+    let Name, imageUrl, type, power, toughness, rarity, setName , text, color, colorStr, flavor, artist, cost, url = '';
+    let costImg = [];
+    let costNum = [];
 
     if(card) {
       // console.log('card:', card);
@@ -47,33 +49,61 @@ export default class DetailPage extends Component {
       toughness = card.card.toughness;
       rarity = card.card.rarity;
       setName = card.card.setName;
-      text = card.card.text
+      text = card.card.text;
       color = card.card.colors;
+      flavor = card.card.flavor;
+      artist = card.card.artist;
+      cost = card.card.manaCost;
+      url = `https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=mtg+${Name.split(' ').join('+')}`;
       if(color){
         color.length == 1 ? (colorStr=color[0]): (colorStr=color[0]+'/'+color[1]);
-      }
-    }
+      };
+      cost.split("").forEach( letter => {
+        if (letter === "G"){
+          costImg.push('http://vignette3.wikia.nocookie.net/mtg/images/f/f7/Mana_G.png/revision/latest?cb=20130604114032');
+        } else if(letter === "W"){
+          costImg.push("http://vignette4.wikia.nocookie.net/mtg/images/d/da/Mana_W.png/revision/latest?cb=20130604114009");
+        } else if(letter === "B"){
+          costImg.push("http://vignette2.wikia.nocookie.net/mtg/images/a/a6/Mana_B.png/revision/latest?cb=20130604114019");
+        } else if(letter === "U"){
+          costImg.push("http://vignette2.wikia.nocookie.net/mtg/images/a/a8/Mana_U.png/revision/latest?cb=20130604114015");
+        } else if(letter === "R"){
+          costImg.push("http://www.slightlymagic.net/forum/download/file.php?id=12830");
+        } else if (letter !== "{" && letter !== "}"){
+          costNum.push(letter);
+        }
 
-    return (
-      <div className='container center'>
-        <div className ="detail row">
-          <h3 className ="title" >{Name}</h3>
-          <div className ="col-xs-6 card card-container">
-            <img src={imageUrl} alt={Name}/>
-            <Link  to='/deck' className="btn btn-default button" onClick={this.addToDeck}>Add to Deck</Link>
-          </div>
-          <div className ="col-xs-6 card card-container des">
-            <ul className ="list-group">
-              <li className = "list-group-item"><b>Type: </b>{type}</li>
-              <li className = "list-group-item"><b>Power and Toughness:</b> {power}/{toughness}</li>
-              <li className = "list-group-item"><b>Colors: </b>{colorStr}</li>
-              <li className = "list-group-item"><b>Rarity: </b>{rarity}</li>
-              <li className = "list-group-item"><b>Set: </b>{setName}</li>
-              <li className = "list-group-item">{text}</li>
-            </ul>
+      });
+      }
+
+      return (
+        <div className='container center'>
+          <div className ="detail row">
+            <h3 className ="title" >{Name}</h3>
+            <div className ="col-xs-6 card card-container">
+              <img src={imageUrl} alt={Name}/>
+              <Link  to='/deck' className="btn btn-default button" onClick={this.addToDeck}>Add to Deck</Link>
+              <a href={url} target="_blank"><img src="https://1.bp.blogspot.com/-xXJU6yWkjkM/V14WGAKkAVI/AAAAAAAAIMw/mh1ykJweeBodMLUEcL_BxIcUNE860_NCQCLcB/s1600/amazon-buy-button-png1.png" className="amazon" alt="Buy on Amazon"/></a>
+            </div>
+            <div className ="col-xs-6 card card-container des">
+              <ul className ="list-group">
+                <li className = "list-group-item"><b>Type: </b>{type}</li>
+                <li className = "list-group-item"><b>Power and Toughness:</b> {power}/{toughness}</li>
+                <li className = "list-group-item"><b>Cost: </b>
+                {costNum.map((num, i) => {return (<span key ={i} className="btn btn-default btn-sm" disabled>{num}</span> )})}
+                {costImg.map((img, i) => {return (<img className = "cost" src={img} key={i}/> )})}</li>
+                <li className = "list-group-item"><b>Colors: </b>{colorStr}</li>
+                <li className = "list-group-item"><b>Rarity: </b>{rarity}</li>
+                <li className = "list-group-item"><b>Set: </b>{setName}</li>
+                <li className = "list-group-item"><b>Text: </b>{text}</li>
+                <li className = "list-group-item"><b>Flavor: </b>{flavor}</li>
+                <li className = "list-group-item"><b>Artist: </b>{artist}</li>
+
+
+              </ul>
+            </div>
           </div>
         </div>
-      </div>
-    )
+      )
+    }
   }
-}
